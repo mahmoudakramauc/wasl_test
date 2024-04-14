e8d1afa2dda33c6ec7a08bc1c7bee17d2dfd0838
mahmoudakramauc <36908660+mahmoudakramauc@users.noreply.github.com>
2024-04-14 14:30:00 -0700

    Delete islamic_scholars_platform directory

--- islamic_scholars_platform/scholars/admin.py
+++
@@ -1,8 +0,0 @@
-# scholars/admin.py
-from django.contrib import admin
-from .models import Scholar, Lecture, Session, Availability
-
-admin.site.register(Scholar)
-admin.site.register(Lecture)
-admin.site.register(Session)
-admin.site.register(Availability)
